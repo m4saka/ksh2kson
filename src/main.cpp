ae49697fb3339cb9eb0c0285923e3aa593fe157c
masaka <masaka@mz-kb.com>
2019-09-21 18:11:38 +0900

    Use filename of argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include "ksh/playable_chart.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
-    ksh::PlayableChart chart("catsjumpanywhere_ex.ksh");
-    std::cout << chart.comboCount() << std::endl;
+    if (argc >= 2)
+    {
+        ksh::PlayableChart chart(argv[1]);
+
+        // TODO: Convert to kson
+        std::cout << chart.comboCount() << std::endl;
+    }
+    else
+    {
+        std::cerr <<
+            "Usage:\n"
+            "./ksh2kson [ksh file]" << std::endl;
+    }
     return 0;
 }
